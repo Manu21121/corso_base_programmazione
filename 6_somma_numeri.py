b6692bfb11a3b0a32b87eebb46319d0ebfdd3883
Manu21121 <manuelmagazzino8@gmail.com>
2022-10-12 19:17:59 +0200

    (ADD) eseguo il mio sesto programma

--- 6_somma_numeri.py
+++ 6_somma_numeri.py
@@ -5,11 +5,11 @@ N3= int(input("inserire il terzo numero: "))
 Somma= int((N1 + N2 + N3))
 if (Somma > 10):
     if (N1 > N2 and N1 > N3):
-        print("il numero più grande è ", N1)
+        print("il numero più grande è il primo: ", N1)
     else:
-        if (N2 > N3):
-            print("il numero più grande è ", N2)
+        if (N2 > N1 and N2 > N3):
+            print("il numero più grande è il secondo: ", N2)
         else:
-            print("il numero più grande è ", N3)
+            print("il numero più grande è il terzo: ", N3)
 else:
     print("La somma dei tre numeri è ", Somma)
