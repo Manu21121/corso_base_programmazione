2b0e06d1d65cef170435497cfb00778508f8464f
Manu21121 <manuelmagazzino8@gmail.com>
2022-10-28 18:02:09 +0200

    (ADD) eseguo il mio secondo programma

--- 2_lato_quadrato.py
+++ 2_lato_quadrato.py
@@ -1,5 +1,5 @@
-LatoQuadrato = int(input("inserisci la lunghezza del lato del quadrato: "))
-Perimetro = LatoQuadrato * 4
-Area = LatoQuadrato * LatoQuadrato
-print("Il perimeto del quadrato è: ", Perimetro)
-print("L'area del quadrato è: ", Area)
+lato_quadrato = int(input("inserisci la lunghezza del lato del quadrato: "))
+perimetro = lato_quadrato * 4
+area = lato_quadrato * lato_quadrato
+print("Il perimeto del quadrato è: ", perimetro)
+print("L'area del quadrato è: ", area)
